d37338492164fa771fe054d9387740ee17a17d62
dnahstar <dnahstar@gmail.com>
2026-03-31 14:05:06 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 깃허브 페이지 배포를 위해 정적 파일 생성을 활성화합니다.
   output: 'export',
-  basePath: process.env.GITHUB_ACTIONS ? '/pioneers-ring-catcher' : '',
+  // 이미지 최적화 기능이 깃허브 서버에서는 작동하지 않으므로 끕니다.
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
