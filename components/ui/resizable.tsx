e23f26b570ab60bb3220e12a065fc00d32c1b194
dnahstar <dnahstar@gmail.com>
2026-04-01 12:11:03 +0900

    Update resizable.tsx

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -4,36 +4,28 @@ import * as React from "react"
 import * as ResizablePrimitive from "react-resizable-panels"
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = ({
-  className,
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+// 타입을 직접 지정하지 않고 라이브러리에서 제공하는 컴포넌트를 그대로 가져와 확장합니다.
+const ResizablePanelGroup = (props: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
   <ResizablePrimitive.PanelGroup
+    {...props}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      className
+      props.className
     )}
-    {...props}
   />
 )
 
 const ResizablePanel = ResizablePrimitive.Panel
 
-const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
-  withHandle?: boolean
-}) => (
+const ResizableHandle = (props: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & { withHandle?: boolean }) => (
   <ResizablePrimitive.PanelResizeHandle
+    {...props}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      className
+      props.className
     )}
-    {...props}
   >
-    {withHandle && (
+    {props.withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
         <div className="h-2.5 w-0.5 bg-foreground/20" />
       </div>
