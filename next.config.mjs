2bb3e9cb0608f9aecafa7cbf321c652f36058bc2
dnahstar <dnahstar@gmail.com>
2026-04-02 14:54:49 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // 정적 배포를 위해 반드시 필요
-  basePath: '/pioneers-ring-catcher', // 상세 주소 경로를 인식하게 만드는 핵심!
+  basePath: '/ring-catcher-game', // 새로운 저장소 이름으로 변경!
+  assetPrefix: '/ring-catcher-game', // 자원 경로도 함께 맞춰줍니다
   images: {
     unoptimized: true, // 이미지 최적화 에러 방지
   },
