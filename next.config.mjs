ebf5c6c4d1ecfc86fc00242fdd8f8b50c06fb058
dnahstar <dnahstar@gmail.com>
2026-04-03 16:15:03 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  // 사용자님의 GitHub Pages 주소가 dnahstar.github.io 이므로 
+  // basePath와 assetPrefix는 비워두는('') 것이 정답입니다.
   basePath: '', 
   assetPrefix: '',
 };
+
 export default nextConfig;
