5a90a1a5fb5a1f2b93614eacf8de724507812aa1
dnahstar <dnahstar@gmail.com>
2026-04-04 04:42:24 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script"; // Script 컴포넌트 추가
 
 export const metadata = {
   title: "PIONEERS!! - Ring Catcher",
@@ -9,8 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
-        {/* 파이 네트워크 SDK 스크립트 (필수!) */}
-        <script src="https://sdk.minepi.com/pi-sdk.js" defer></script>
+        {/* 파이 네트워크 SDK 스크립트 (Next.js 권장 방식) */}
+        <Script 
+          src="https://sdk.minepi.com/pi-sdk.js" 
+          strategy="beforeInteractive" 
+        />
       </head>
       <body>
         {children}
