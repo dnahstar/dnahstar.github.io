51f25d0e5d81927dfc18ce0b67f2c7a438704547
dnahstar <dnahstar@gmail.com>
2026-04-04 06:42:58 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Pi-Ring 2.0",
+  title: "Pi-Ring 2.0 Mainnet Edition",
   description: "Grit leads to the Mainnet",
 };
 
@@ -9,10 +9,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
-        {/* Pi SDK를 미리 로드하여 인증 누락 방지 */}
-        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+        {/* 외부 SDK를 최상단에서 로드하여 인증 팝업 누락 방지 */}
+        <script src="https://sdk.minepi.com/pi-sdk.js" defer></script>
       </head>
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
